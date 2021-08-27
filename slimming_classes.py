0a0d44a1766a0cf347de8514a892d6ed296c3676
Mohamed Aly <mohamedaymanali@gmail.com>
2021-08-27 16:59:58 +0100

    More complete Sample, NtupleFile classes and a new UprootArgs class

--- slimming_classes.py
+++ slimming_classes.py
@@ -10,63 +10,125 @@ to perform the slimming with Uproot4
 
 '''
 
+from common_tools import combine_dicts
+from common_classes import Branch
 
-# This is the highest-level class -- hidden from user
+
+# This is the LOWEST-level class -- hidden from user
 class NtupleFile:
-	def __init__(self, file_path, tree_name='nominal_Loose', branches_on=[], branches_off=[]):
-		self.file_path = file_path
+	def __init__(self, ntuples_dirs, dsids, exclude=[], tree_name='', branches_to_keep=[], branches_to_drop=[]):
+		self.paths = self.get_path(ntuples_dirs, dsids, excluded_files=exclude)
 		self.tree = tree_name
-		self.branches_on = branches_on
-		self.branches_off = branches_off
+		self.branches = get_branches_filter(branches_to_keep, branches_to_drop)
+
+	def get_paths(dirs, dsids, excluded_files=[]):
+	    from glob import glob
+	    paths = []
+	    for directory in dirs:
+	        for dsid in dsids:
+
+	            general_path = glob(directory + '*' + dsid + '*')
+	            paths_to_keep = set(general_path)
+	            for excluded_file in excluded_files:
+	                excluded_dir = glob(directory + '*' + excluded_file + '*')
+	                paths_to_keep -= set(excluded_dir)
+	            paths_to_keep = list(paths_to_keep)
+	            paths.append(paths_to_keep)
+	    return paths
+
+	def get_branches_filter(branches_to_keep, branches_to_drop):
+		branches = []
+		for idx, branch in enumerate(branches_to_keep):
+			name = branch.name
+			positive_regex = '/'
+			if "*" in name and ".*" not in name:
+				name = name.replace("*", ".*")
+			positive_regex += name
+	    	positive_regex += '/i'
+	    	branch.set_filter(positive_regex)
+	    	branches.append(branch)
+
+		for idx, branch in enumerate(branches_to_drop):
+			name = branch.name
+			negative_regex = '/^((?!'
+			if "*" in name and ".*" not in name:
+				name = name.replace("*", ".*")
+			negative_regex += name
+	    	negative_regex += ').)*$/i'
+	    	branch.set_filter(negative_regex)
+	    	branches.append(branch)
+
+		for branch in branches_to_make:
+			recipe = br.expression
+			branch.set_filter(recipe)
+			branches.append(branch)
+		return branches
+
+
+# This is a HIGHER-level class -- filled by user in config
+class Sample:
+
+	def __init__(self, where_ntuples_at=[], exclude_files=[], common_branches={},
+				          dsids=[], cuts=[], add_branches={},
+				          branch_like_nom=False, systematics={}):
+
+		self.location = where_ntuples_at
+		self.ignored_files = exclude_files
+		self.dsids = dsids
+		self.selection = cuts
+		self.trees_to_branches_map = combine_dicts(dicts=[common_branches, add_branches])  # Need later check that new branches have epxressions
+		self.all_like_nominal = branch_like_nom
+		self.ntuples = []
+
+		for tree, branches in self.trees_to_branches_map:
+			br_to_keep = [br in branches if br.status == 'on']
+			br_to_drop = [br in branches if br.status == 'off']
+			br_to_make = [br in branches if br.status == 'new']
+			ntuple_obj = NtupleFile(self.location, self.dsids, exclude=self.ignored_files, tree_name=tree, branches_to_keep=br_to_keep, branches_to_drop=br_to_drop, branches_to_make=br_to_make)
+			self.ntuples.append(ntuple_obj)
+		self.args = self.get_uproot_args()
 
 	def get_uproot_args(self):
-		branches_on = self.branches_on
-		branches_off = self.branches_off
-		fp = self.file_path
-		tree_name = self.tree_name
-		any_not_off_branch = '(' + f'/^(?!.*_({ branches_off.join('|')})).*' + ')'
-		any_on_branch = '(' + branches_on.join('|') + ')'
-		branches_filter = any_on_branch+'|'+any_not_off_branch
-		files = file_path+":"+tree_name
-		return files, branches_filter
+		all_sample_args = []
+		for ntuple in ntuples:
+			path_to_tree = []
+			paths = ntuple.paths
+			tree_name = ntuple.tree
+			branches = ntuple.branches
+			for path in paths:
+				path_to_tree = path+':'+tree_name
+
+			args = UprootArgs(paths, branches, cuts)
+		all_sample_args.append(args)
+
+		return all_sample_args
+
+	# If a new branch is defined, we may want to update its name from
+	# the expression to the name specified in config
+	def update_new_branch_name(self, branch, new_name):
+		if not isinstance(branch, Branch):
+			raise ValueError("Must pass a Branch object")
+		return pass
+
+	def add_branch(self, branch):
+
+		if not isinstance(branch, Branch):
+			raise ValueError("Branch name must be a string")
+
+		return pass
+
+	def remove_branch(self, branch_name):
+
+		if not isinstance(branch, str):
+			raise ValueError("Branch name must be a string")
+
+		return pass
 
 
-# This is a lower-level class -- filled by user in config
-class Sample:
-	# can pass sth like
-	# {nominal_loose: [(branch_name, on), (branch_name2, on), (branch_name3, off)]} # before logoff on Fri 20.08
-	def __init__(self, dsid_to_filepaths_map, trees_to_keep=[], branches_on=[], branches_off=[]):
-
-		full_paths = self.make_full_filepaths(type(dsid_to_filepaths_map))
-		self.ntuplefile = {tree: NtupleFile(full_paths, tree, ) for tree in trees_to_keep}
-		
-		self.file_path = self.make_full_filefile_path
-		self.trees = trees_to_keep
-		self.keep_branches = branches_on
-		self.drop_branches = branches_off
-
-	def make_full_filepaths(self, mapping_type):
-		if isinstance(dsid_to_filepaths_map, dict):
-			# Do dict magic?
-			pass
-		elif isinstance(dsid_to_filepaths_map, str):
-			# Lad is giving a file?
-			if ".txt" in dsid_to_filepaths_map:
-				# Deal with it as text
-				pass
-			elif ".csv" in dsid_to_filepaths_map:
-				# Deal with it as CSV
-				pass
-			elif ".json" in dsid_to_filepaths_map:
-				# Deal with it as JSON
-				pass
-			else:
-				# Logging here to give error of unsupported mapping file
-				pass
-		else:
-			# Logging here to give error of unupported mapping type
-			pass
-
-		dsid_to_filepaths_map = self.dsid_to_filepaths_map
-		full_paths = [fp+dsid+"*" for dsid, fp in dsid_to_filepaths_map.items()]
-		return full_paths
+class UprootArgs:
+	def __init__(self, files, branches, cuts, step_size='4 GB'):
+		self.files = files
+		self.cuts = cuts
+		self.filter_name = [br.filter for br in branches]
+		self.branches = branches
+		self.stepsize = step_size
