e3f0b9d83b92901701a552bc684d5b4967ea28a4
Mohamed Aly <mohamedaymanali@gmail.com>
2021-08-27 17:03:40 +0100

    Commenting incomplete systematic class

--- common_classes.py
+++ common_classes.py
@@ -1,5 +1,24 @@
 class Branch:
 
+def __init__(self, name, status, expression=''):
+	self.name = name
+	if status != 'on' and status != 'off' and status != 'new':
+		raise ValueError("Invalid Branch Status")
+	else:
+		self.status = status
+
+	if status == 'new' and expression == '':
+		raise ValueError("New Branch declared but no expression given")
+	elif status != 'new' and expression != '':
+		raise ValueError("Expression given but Branch not declared as new")
+	elif status == 'new' and expression != '':
+		self.expression = expression
+	else:
+		self.expression = None
+	self.filter = None # To be set with a setter. 
 
 
-class Systematic:
+def set_filter(self, filter_expression):
+	self.filter = filter_expression
+
+# class Systematic:
