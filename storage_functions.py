d7b1aad6c9047b8664e75a5a55dfc236cc3a7033
Konrad Dariusz Malirz <kmalirz@lxplus706.cern.ch>
2021-11-03 03:43:41 +0100

    fix to dask stuff

--- storage_functions.py
+++ storage_functions.py
@@ -10,6 +10,9 @@ import dask.dataframe as dd
 import timeit
 from hist import Hist
 import time
+import gc
+import psutil
+from dask.distributed import get_client
 
 class HistoMaker:
 
@@ -18,10 +21,10 @@ class HistoMaker:
         self.folder_name = kwargs.get('folder_name',None)
         self.file_list = kwargs.get('file_list',None)
         self.client_params = kwargs.get('client_params',{}) ## no params by default
-        self.dask_dfs = kwargs.get('dask_data_frames',[]) ## no data frames present by default
         self.hist_names = kwargs.get('histogram_names',[])
         self.hist_params = kwargs.get('histogram_params',{})
-        self.client = None
+        self.worker_number = 1 # default
+        
     
     def get_att(self):
         return vars(self)
@@ -32,35 +35,42 @@ class HistoMaker:
 
     def client_start(self,**kwargs):
         for key in kwargs:
-            if self.client_params.get(key) != None:
-                client_params[key] = kwargs[key]
+            if self.client_params.get(key) != kwargs[key]:
+                self.client_params[key] = kwargs[key]
 
         cl = dask.distributed.Client(**self.client_params)
-        self.client = cl
+        self.worker_number = len(cl.scheduler_info()['workers'])
         return cl
-
+    
+    @dask.delayed
+    def load(self,file_path):
+        temp = pd.read_hdf(file_path)
+        return temp
+    
+    @dask.delayed
+    def fill(self,histogram,data):
+        histogram.fill(data)
+        return histogram
     
     def load_data(self,**kwargs):
         ## change that to something better in the future
         if kwargs.get('folder_name',None) != None: self.folder_name = kwargs.get('folder_name',None)
         if kwargs.get('file_list',None) != None: self.file_list = kwargs.get('file_list',None)
-        #if kwargs.get('drop_existing',False) == True: self.dask_dfs = [] ##in the future expand on these options
-
+        
+        counter = 1
+        results = []
+        key_list = list(self.histograms.keys())
+          
         for f in self.file_list:
-            t1 = time.time()
-            temp = pd.read_hdf( self.folder_name + '/' + f)
-            t2 = time.time()
-            print("read time:", t2-t1)
-            temp.reset_index(inplace = True)
-            t3 = time.time()
-            print("reset time:", t3-t2)
-            temp = dd.from_pandas(temp,npartitions=1) ##change to variable with list option or just one value
-            self.dask_dfs.append(temp)
-            t4 = time.time()
-            print("converting:", t4-t3)
-
-        return self.dask_dfs
-
+                
+            load_result = self.load(self.folder_name + '/' + f)
+            result = self.fill(self.histograms[key_list[counter - 1]],load_result[kwargs['data_col']])
+            results.append(result)
+            counter += 1
+                
+        return results
+    
+    
     def create_histograms(self,**kwargs):
         if kwargs.get('histogram_names',None) != None: self.hist_names = kwargs.get('histogram_names',None)
         if kwargs.get('histogram_params',None) != None: self.hist_params = kwargs.get('histogram_params',None)
@@ -71,21 +81,6 @@ class HistoMaker:
 
         return self.histograms
     
-    
-    def fill_histograms(self,col_name,**kwargs): ##change that later
-        counter = 0
-        for key in self.histograms:
-            self.histograms[key].fill(self.dask_dfs[counter][col_name])
-            counter += 1
-
-        return self.histograms
-
-
-
-file_list = ["test0.h5","test1.h5","test12345.h5"]
-folder = "Skl_Data"
-storage_path = "Skl_Data/storage_test.h5"
-
 
 
 
