a5552d02f24a6ed924d9d3a33e6d55236805dbd1
Flavio Ryu Ciciriello <flciciri@lxplus740.cern.ch>
2021-11-30 04:07:19 +0100

    Added jupyter notebook to reproduce prefit plots graphs using matplotlib

--- yaml_scripts_tests/yamlscript_histdata.py
+++ yaml_scripts_tests/yamlscript_histdata.py
@@ -1,5 +1,6 @@
 import yaml
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def histdata_parser(file_name):
@@ -22,13 +23,18 @@ def histdata_parser(file_name):
         sum = pd.concat([samples_df, total_df, data_df], keys=['Samples', 'Total', 'Data'])
         # at this point, rows in sum are list of 10 numbers, each corresponding
         # to a different yield -> need to split them and put them into individual columns
+        
         temp_df = pd.DataFrame(sum['Yield'].to_list(), columns=[f"Yield_{i}" for i in range(len(sum.iloc[0][0]))])
         # match index of temp_df and sum to allow concat later
         temp_df.set_index(sum.index, inplace=True)
         sum = pd.concat([sum, temp_df], axis=1).drop('Yield', axis=1)
-
+        
         print(sum)
         print(figure_df)
+
+        # nbins = 10
+        # fig, ax = plt.subplots()
+        # sum.plot.hist(bins=nbins)
 
 
 if __name__ == "__main__":
