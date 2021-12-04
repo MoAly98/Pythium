d3beb48fe11a0a70faf0b5a14bc14487d65b7424
Konrad Dariusz Malirz <kmalirz@lxplus7107.cern.ch>
2021-12-04 15:14:19 +0100

    added comments

--- storage_functions.py
+++ storage_functions.py
@@ -87,7 +87,7 @@ class HistoMaker:
         self.histograms_computed = [] 
         self.histogram_variables = kwargs.get('histogram_variables',{})
 
-    def create_file_list(self,top_directory = os.getcwd(),file_regex = '(?=^[^.].)(.*pkl$)|(?=^[^.].)(.*h5$)',dir_regex = '(?=^[^.].)',**kwargs) -> List[str]: 
+    def create_file_list(self,top_directory = os.getcwd(),file_regex = '(?=^[^.].)(.*pkl$)|(?=^[^.].)(.*h5$)',dir_regex = '(?=^[^.].)',**kwargs): 
         """
         creates a list of files in directories under root directory that match regex strings.
 
@@ -146,17 +146,57 @@ class HistoMaker:
    
     @dask.delayed
     def load_h5(self,file_path):
+        """
+        loads data from hdf file
+
+        Arguments
+        -------
+        file_path :
+            full file path
+
+        Return
+        -------
+        Pandas DataFrame object (delayed)
+        """
         temp = pd.read_hdf(file_path)
         return temp
     
     @dask.delayed
     def load_pkl(self,file_path):
+        """
+        loads data from pickle file
+
+        Arguments
+        -------
+        file_path :
+            full file path
+
+        Return
+        -------
+        Pandas DataFrame object (delayed)
+        """
         temp = pd.read_pickle(file_path)
         return temp
     
     @dask.delayed
     def fill(self,data,hist_dict):
+        """
+        fills histograms with data from input DataFrame
+
+        Arguments
+        -------
+        data :
+            Pandas/Dask DataFrame
         
+        hist_dict : 
+            dictionary that contains hist vars. Each key corresponds to column name 
+            and each value contains hist object params for example
+            'rljet_m_comb[:,0]' : Regular(20, 50e3, 500e3,  name='x', label='m [MeV]').
+
+        Return
+        -------
+        List of histogram objects (delayed)
+        """
         column_list = list(data.columns)
         column_dict = {}
 
@@ -179,7 +219,21 @@ class HistoMaker:
         return histograms_filled
     
     def load_and_fill(self,file_list = [],**kwargs):
+        """
+        combines load and fill for a list of files
+
+        Arguments
+        -------
+        file_list :
+            list of files to load and fill 
         
+        **kwargs : 
+            keyword arguments
+
+        Return
+        -------
+        List of delayed objects that contain load and fill functions
+        """
         results = []
         for f in file_list:
             
@@ -195,6 +249,26 @@ class HistoMaker:
         return results
     
     def compute_histograms(self,chunk_size = 8,file_list = [],**kwargs):
+        """
+        computes delayed objects in chunks and appends them to the histograms_computed variable
+
+        Arguments
+        -------
+        chunk_size : 
+            number of files to be loaded and filled in one batch
+
+        file_list :
+            list of files to load and fill 
+        
+        **kwargs : 
+            keyword arguments
+
+        Return
+        -------
+        list of lists of hist objects such that histograms_computed[n]
+        returns nth file that contains a number of hist objects such that, 
+        histograms_computed[n][i] returns ith histogram.
+        """
         output = []
 
         if file_list == []: file_list = self.file_list
@@ -219,6 +293,18 @@ class HistoMaker:
 
 
 def combine_dicts(dict_list):
+    """
+    combine dictionaries
+
+    Arguments
+    -------
+    dict_list : 
+        list of dictionaries to combine
+
+    Return
+    -------
+    combined dictionary
+    """
     out = {}
     for dictionary in dict_list:
         for key in dictionary:
