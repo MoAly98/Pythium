4e52e328cdfce5734a39d1606df9d7d9b8843ee7
Flavio Ryu Ciciriello <flciciri@lxplus799.cern.ch>
2022-02-21 02:17:48 +0100

    Various changes to pull plot file

--- yaml_scripts_tests/plot_classes.py
+++ yaml_scripts_tests/plot_classes.py
@@ -8,6 +8,7 @@ import mplhep as hep
 
 class EmptyPlot():
     
+    
     def __init__(self, title=None, layout=(1,1), size=(6.4,4.8), style="ATLAS"):
         
         self.title   = title
@@ -19,28 +20,35 @@ class EmptyPlot():
         self.container2d = []
         self.colorlist   = []
     
+    
     def create_canvas(self):
         self.fig = plt.figure(figsize=self.figsize)
         hep.style.use(self.style) # available syles are: {"ALICE" | "ATLAS" | "CMS" | "LHCb1" | "LHCb2"}
 
+        
     def make_grid(self, **grid_kw):
         self.gs = gridspec.GridSpec(self.layout[0], self.layout[1], **grid_kw) # there has to be a smarter way to unpack tuple
+    
     
     def make_subplot(self, start_row, end_row, start_col, end_col, **subplot_kw): # these args must be allowed by layout size
         return self.fig.add_subplot(self.gs[start_row:end_row,start_col:end_col], **subplot_kw)
     
+    
     def set_majorticks(self, ax, **ticks_kw):
         ax.tick_params(**ticks_kw)
+    
     
     def set_minorticks(self, ax, **ticks_kw):
         ax.minorticks_on()
         ax.tick_params(**ticks_kw)
+    
     
     def set_color(self, colormap='viridis', reverse=False):
         self.user_cmap = colormap
         if reverse:
             self.user_cmap = self.user_cmap + '_r'
         mpl.rcParams['image.cmap'] = self.user_cmap
+    
     
     def set_stack_color(self, colormap="viridis", reverse=False):
         # create custom color map for stacked histogram
@@ -63,6 +71,7 @@ class EmptyPlot():
 
         self.colorlist = clist
     
+    
     def config_rcParams(self, settings_dict):
         # let user update global rcParams values of matplotlib
         if isinstance(settings_dict, dict):
@@ -72,6 +81,8 @@ class EmptyPlot():
             print("Please put a dictionary as argument for 'config_rcParams' function") # need logger here
         
         # can also add a 'help' command to display all the rcParams variables that can be cahnged to help user
+        # there is an easier function already in matplotlib, might want to switch to that
     
-    def saveimage(self, name):
-        self.fig.savefig(name, facecolor='white', transparent=False, dpi=1200, bbox_inches='tight')
+    
+    def saveimage(self, name, dpi=1200):
+        self.fig.savefig(name, facecolor='white', transparent=False, dpi=dpi, bbox_inches='tight')
