3ad126c197c84d12f8e44d6f6a92827bdb73c0b6
Flavio Ryu Ciciriello <flciciri@lxplus789.cern.ch>
2022-02-24 22:50:26 +0100

    Mid-work commit

--- plotting_misc/plot_classes.py
+++ plotting_misc/plot_classes.py
@@ -23,31 +23,57 @@ class EmptyPlot():
         self.container1d = []
         self.container2d = []
         self.colorlist   = []
+        
+        # set default dict of axes titles
+        self.xtitles_dict = {"xmain": '',
+                             "xtop" : '',
+                             "xbot" : '',
+        }
+        self.ytitles_dict = {"ymain": '',
+                             "ytop" : '',
+                             "ybot" : '',
+        }
     
     
     def create_canvas(self):
+        
         self.fig = plt.figure(figsize=self.figsize)
         hep.style.use(self.style) # available syles are: {"ALICE" | "ATLAS" | "CMS" | "LHCb1" | "LHCb2"}
 
         
     def make_grid(self, **grid_kw):
+        
         self.gs = gridspec.GridSpec(self.layout[0], self.layout[1], **grid_kw) # there has to be a smarter way to unpack tuple
     
     
     def make_subplot(self, start_row, end_row, start_col, end_col, **subplot_kw): # these args must be allowed by layout size
+        
         return self.fig.add_subplot(self.gs[start_row:end_row,start_col:end_col], **subplot_kw)
     
     
     def set_majorticks(self, ax, **ticks_kw):
+        
         ax.tick_params(**ticks_kw)
     
     
     def set_minorticks(self, ax, **ticks_kw):
+        
         ax.minorticks_on()
         ax.tick_params(**ticks_kw)
+        
     
+    def set_xtitles(self, ax, axis_key):
+        
+        ax.set_xlabel(self.xtitles_dict[axis_key])
+    
+    
+    def set_ytitles(self, ax, axis_key):
+        
+        ax.set_ylabel(self.ytitles_dict[axis_key])
+        
     
     def set_color(self, colormap='viridis', reverse=False):
+        
         self.user_cmap = colormap
         if reverse:
             self.user_cmap = self.user_cmap + '_r'
@@ -78,16 +104,20 @@ class EmptyPlot():
     
     def config_rcParams(self, settings_dict):
         # let user update global rcParams values of matplotlib
+        
         if isinstance(settings_dict, dict):
             for key, value in settings_dict.items():
                 mpl.rcParams[key] = value
+                print(key, value)
         else:
-            print("Please put a dictionary as argument for 'config_rcParams' function") # need logger here
+            print("Please put a dictionary as argument for 'config_rcParams' function") # need logger
         
         # can also add a 'help' command to display all the rcParams variables that can be cahnged to help user
+        
         # there is an easier function already in matplotlib, might want to switch to that
         # https://matplotlib.org/stable/api/matplotlib_configuration_api.html#matplotlib.rc
     
     
     def saveimage(self, name, dpi):
+        
         self.fig.savefig(name, facecolor='white', transparent=False, dpi=dpi, bbox_inches='tight')
