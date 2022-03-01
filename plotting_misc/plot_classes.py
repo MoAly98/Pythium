8360bfaad7a0b976074414616486118bf3d28ecd
Flavio Ryu Ciciriello <flciciri@lxplus744.cern.ch>
2022-03-01 01:33:36 +0100

    Various improvements on all plots

--- plotting_misc/plot_classes.py
+++ plotting_misc/plot_classes.py
@@ -3,6 +3,8 @@ import matplotlib.gridspec as gridspec
 import matplotlib as mpl
 import numpy as np
 import mplhep as hep
+import boost_histogram as bh
+import pandas as pd
 
 
 """
@@ -13,57 +15,71 @@ This is where all plot classes will be stored
 class EmptyPlot():
     
     
-    def __init__(self, title=None, layout=(1,1), size=(6.4,4.8), style="ATLAS"):
+    def __init__(self, title="", layout=(1,1), size=(6.4,4.8), style="ATLAS"):
         
-        self.title   = title
-        self.layout  = layout
-        self.figsize = size
-        self.style   = style
+        self.master_title = title
+        self.layout       = layout
+        self.figsize      = size
+        self.style        = style
         
-        self.container1d = []
-        self.container2d = []
-        self.colorlist   = []
+        self.active_data = None
+        self.data_container  = [] # allow to store multiple objects
         
         # set default dict of axes titles
         self.xtitles_dict = {"xmain" : '',
                              "xtop"  : '',
                              "xbot"  : '',
-                             "xleft" : '',
+                             "xleft" : '', # not used
                              "xright": ''
                             }
         self.ytitles_dict = {"ymain" : '',
                              "ytop"  : '',
                              "ybot"  : '',
-                             "yleft" : '',
+                             "yleft" : '', # not used
                              "yright": ''
                             }
+        """
+        The terms 'main', 'top', 'bot' etc. refer to the follownig subplot scheme:
+        
+                 -------- 
+                |  top   |
+                 --------        
+         -----   --------   -----
+        |     | |        | |     |
+        |left | |  main  | |right|
+        |     | |        | |     |
+         -----   --------   -----
+                 -------- 
+                |  bot   |
+                 --------
+        
+        And each plot type (ratio, pull, projection, corrm) will be a combination of
+        these subplots (Ax objects). For example, the ratio plot will be made of main
+        and bot subplots and the projection plot will be made of main, top and right
+        subplots. The left subplot is currently not used and might be deleted in the 
+        future if no use cases are found.
+        
+        The space between the subplots is called 'spacing' throughout all classes
+        """
     
     
     def create_canvas(self):
+        # create figure and set the hep style (rcParams)
         
         self.fig = plt.figure(figsize=self.figsize)
         hep.style.use(self.style) # available syles are: {"ALICE" | "ATLAS" | "CMS" | "LHCb1" | "LHCb2"}
 
         
     def make_grid(self, **grid_kw):
+        # create gridspec object to set configuration of subplots
         
         self.gs = gridspec.GridSpec(self.layout[0], self.layout[1], **grid_kw) # there has to be a smarter way to unpack tuple
     
     
     def make_subplot(self, start_row, end_row, start_col, end_col, **subplot_kw): # these args must be allowed by layout size
+        # generate ax objects (subplots)
         
         return self.fig.add_subplot(self.gs[start_row:end_row,start_col:end_col], **subplot_kw)
-    
-    
-    def set_majorticks(self, ax, **ticks_kw):
-        
-        ax.tick_params(**ticks_kw)
-    
-    
-    def set_minorticks(self, ax, **ticks_kw):
-        
-        ax.minorticks_on()
-        ax.tick_params(**ticks_kw)
     
     
     def fill_titlesdict(self, **titles_kw):
@@ -83,13 +99,17 @@ class EmptyPlot():
         
     
     def set_xtitles(self, ax, axis_key, fontsize, labelpad):
+        # set x axis labels according to xtitles_dict
         
         ax.set_xlabel(self.xtitles_dict[axis_key], fontsize=fontsize, labelpad=labelpad)
+        print("okx")
     
     
     def set_ytitles(self, ax, axis_key, fontsize, labelpad):
+        # set y axis labels according to ytitles_dict
         
         ax.set_ylabel(self.ytitles_dict[axis_key], fontsize=fontsize, labelpad=labelpad)
+        print("oky")
         
     
     def set_color(self, colormap='viridis', reverse=False):
@@ -98,28 +118,6 @@ class EmptyPlot():
         if reverse:
             self.user_cmap = self.user_cmap + '_r'
         mpl.rcParams['image.cmap'] = self.user_cmap
-    
-    
-    def set_stack_color(self, colormap="viridis", reverse=False):
-        # create custom color map for stack histogram
-        
-        clist = []
-        source = self.dflist # list of units to be stacked
-        pct_max = 99 # max percentile of color ramp
-        pct_min = 1  # min percentile of color ramp
-        cmap = mpl.cm.get_cmap(colormap)
-        # number of items in data source
-        n = len(source)
-        # list of values between 0.00 and 1.00; length equals length of data source
-        sequence = list(i/100 for i in (np.arange(pct_min, pct_max, (pct_max-pct_min)/n)))
-        # reverse if required
-        sequence = sequence if not reverse else reversed(sequence)
-        # create list of colors
-        for i in sequence:
-            color = cmap(i) 
-            clist.append(color)
-
-        self.colorlist = clist
     
     
     def config_rcParams(self, settings_dict):
@@ -141,3 +139,16 @@ class EmptyPlot():
     def saveimage(self, name, dpi):
         
         self.fig.savefig(name, facecolor='white', transparent=False, dpi=dpi, bbox_inches='tight')
+
+        
+    def show_content(self):
+        # print out data content of histogram and returns pandas dataframe
+        
+        if isinstance(self.data, bh.Histogram):
+            pass
+        
+        elif isinstance(self.data, pd.core.frame.DataFrame):
+            print(self.data)
+        
+        else:
+            pass
