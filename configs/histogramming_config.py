73a9afd07283357af1a030451bafc7590ff058cf
Konrad Dariusz Malirz <kmalirz@lxplus755.cern.ch>
2022-04-07 18:48:01 +0200

    cross product updates

--- configs/histogramming_config.py
+++ configs/histogramming_config.py
@@ -1,7 +1,8 @@
 import utils.histogramming.storage_functions as sf
 import configs.hist_vars as hist_vars
-from utils.histogramming.cross_product_backend import XP_Sample, XP_Region, XP_Systematics
-
+from utils.histogramming.cross_product_backend import XP_Sample, XP_Region, XP_Systematics, XP_Overall, XP_Histo
+from hist.axis import Variable, Regular
+from configs.sklim_config import samples as sklim_samples
 
 var_dict = sf.combine_dicts([hist_vars.var_main, hist_vars.var_series, hist_vars.var_beta, 
                                hist_vars.var_ecf_beta, hist_vars.var_dichoric, hist_vars.var_ecfg])
@@ -15,7 +16,7 @@ client_params = {
 }
 
 file_list = {
-    "top_directory" : '/afs/cern.ch/user/k/kmalirz/pythium/temp',
+    "top_directory" : '/afs/cern.ch/user/k/kmalirz/pythium/temp/',
     "file_regex" : '(?=^[^.].)(.*gammajet_210921.*|.*15.*)(.*h5$)'
 }
 
@@ -26,8 +27,12 @@ computation_params = {
 
 out_dir = '/tmp/kmalirz'
 
+Samples_test = [
+    XP_Sample(name = item.name, regex = True, top_directory = file_list['top_directory'], 
+    file_regex = f'(.*{item.name}_chunk.*)(.*h5$)') for item in sklim_samples
+]
 
-Samples = [
+Samples_XP = [
     XP_Sample(name = 'ttb_PP8_AFII', regex = True, top_directory = file_list['top_directory'], 
     file_regex = '(.*ttb_PP8_AFII_chunk.*)(.*h5$)'),
     XP_Sample(name = 'tH', regex = True, top_directory = file_list['top_directory'], 
@@ -47,7 +52,8 @@ Regions = [ #write switch function
 ]
 
 Systematics = [ #do weights systematic
-    XP_Systematics(name = 'test', weighting = 1)
+    XP_Overall(name = 'test', Adjustment = 0.5),
+    XP_Overall(name = 'another test', Adjustment = 0)
 ]
 
 def functional_XP(sample,region,systematic):
--- utils/histogramming/cross_product_backend.py
+++ utils/histogramming/cross_product_backend.py
@@ -30,13 +30,45 @@ class XP_Region:
         self.filter = kwargs.get('filter',None)
         self.name = kwargs.get('name',None)
 
-class XP_Systematics:
+class XP_Systematics: #base systematic class
 
     def __init__(self,**kwargs):
 
         self.name = kwargs.get('name',None)
         self.weighting = kwargs.get('weigth',1)
 
+    def output_weights(self,*args):
+
+        return None
+
+class XP_Overall(XP_Systematics): 
+
+    def __init__(self,**kwargs):
+
+        super().__init__(**kwargs)
+
+        self.change = kwargs.get('Adjustment', 0)
+        self.Excludes = kwargs.get('Excludes', '')
+    
+    def output_weights(self,column_data):
+
+        weights = np.ones(len(column_data))*(1+float(self.change))
+        return weights
+
+class XP_Histo(XP_Systematics):
+
+    def __init__(self,**kwargs):
+
+        super().__init__(self,**kwargs)
+
+        self.formula = kwargs.get('Formula') #pass a lambda function with a dataframe as an input
+        self.Excludes = kwargs.get('Excludes')
+
+    def output_weights(self,column_data):
+
+        weights = self.formula(column_data).to_numpy()
+
+        return weights
 
 class Named_hists:
 
