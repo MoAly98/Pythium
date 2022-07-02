2c955ebd9f12dbb26dcc497a03aabb285dadbf2f
maly <mohamedaymanali@gmail.com>
2022-07-02 18:23:33 +0200

    remove sad attempt at changing theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,6 @@
 
 # Configuration file for the Sphinx documentation builder.
 import os,sys
-import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('../../src/'))
 # -- Project information
 
